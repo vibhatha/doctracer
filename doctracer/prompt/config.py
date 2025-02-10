2657abbee04d7383d7a4b0c5321d40c99f45447f
Zaeema Nashath <96104978+zaeema-n@users.noreply.github.com>
2025-02-10 08:25:04 +0530

    Added extragazette table extraction functionality (#21)
    * Added missing dependencies
    
    * Refactored ExtraGazetteProcessor to ExtraGazetteAmendmentProcessor
    
    * Added extragazette table extraction functionality
    
    * Update README.md

--- doctracer/prompt/config.py
+++ doctracer/prompt/config.py
@@ -4,6 +4,10 @@ class MessageConfig(ABC):
     @abstractmethod
     def get_messages(self, prompt: str):
         raise NotImplementedError("Subclasses should implement this method.")
+    
+    @abstractmethod
+    def get_image_messages(self, prompt: str, image: str):
+        raise NotImplementedError("Subclasses should implement this method.")
 
 class SimpleMessageConfig(MessageConfig):
     def get_messages(self, prompt: str):
@@ -11,3 +15,15 @@ class SimpleMessageConfig(MessageConfig):
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": prompt}
         ]
+    
+    def get_image_messages(self, prompt: str, image: str):
+        return [
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": prompt},
+                    {"type": "image_url", "image_url": {"url": f"data:image/jpeg;base64,{image}"}}
+                ]
+            }
+        ]
+    
